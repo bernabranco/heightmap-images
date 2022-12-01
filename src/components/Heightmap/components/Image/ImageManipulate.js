4912c110793bb3adf4f27817e86ffc6690c55d30
Bernardo Branco <bernardoagbranco@gmail.com>
2022-12-01 13:30:48 +0000

    refactor change image on click

--- src/components/Heightmap/components/Image/ImageManipulate.js
+++ src/components/Heightmap/components/Image/ImageManipulate.js
@@ -1,25 +1,4 @@
-import * as traits from "../../../../utils/Traits";
-
-const LIST_SIZE = traits.image_list_size;
-
-// change image by clicking on it
-export function changeImageOnClick(imagesData, positions, colors, mesh) {
-  for (let i = 0; i < LIST_SIZE; i++) {
-    const image = document.getElementById(`image${i}`);
-    image.onclick = (e) => changeImage(e, imagesData, positions, colors, mesh);
-  }
-}
-
-export function changeImage(e, imagesData, positions, colors, mesh) {
-  console.log("change image");
-  let selectedImage;
-  console.log(imagesData);
-  for (let i = 0; i <= imagesData.length; i++) {
-    if (e.target.id === `image${i}`) {
-      selectedImage = imagesData[i].data;
-    }
-  }
-
+export const updateMeshGeometryProperties = (selectedImage, positions, colors, mesh) => {
   let colorIndex = 0;
   let positionIndex = 0;
   for (let i = 0; i < colors.length; i++) {
@@ -43,19 +22,6 @@ export function changeVideoFrames(
   mesh,
   counter
 ) {
-  let colorIndex = 0;
-  let positionIndex = 0;
   let selectedImage = imagesData[counter].data;
-  for (let i = 0; i < colors.length; i++) {
-    const r = selectedImage[colorIndex + 0] / 255;
-    const g = selectedImage[colorIndex + 1] / 255;
-    const b = selectedImage[colorIndex + 2] / 255;
-    const x = positions[positionIndex + 0];
-    const y = positions[positionIndex + 1];
-    const z = 1.0 + r + g + b;
-    mesh.geometry.attributes.position.setXYZ(i, x, y, z);
-    positionIndex += 3;
-    mesh.geometry.attributes.color.setXYZ(i, r, g, b);
-    colorIndex += 4;
-  }
+  updateMeshGeometryProperties(selectedImage, positions, colors, mesh);
 }
