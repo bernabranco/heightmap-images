94592d353ec4fd4495c520e67fff89a95b7da2c3
Bernardo Branco <bernardoagbranco@gmail.com>
2023-07-31 01:30:10 +0100

    feat: get uploaded images on mount

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,25 @@
-import { React } from "react";
+import { React, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Audio } from "../../components/Menu/components/Audio/Audio";
 import Threejs from "../../components/ImageProcessing/Threejs";
 import Menu from "../../components/Menu/Menu";
+import { useImageContext } from "../../store/ImageContext";
 
 export default function Dashboard() {
+  const { uploadedImages } = useImageContext();
+
+  const navigate = useNavigate();
+
+  const goToStartScreen = () => {
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (uploadedImages.length === 0) {
+      goToStartScreen();
+    }
+  }, [uploadedImages]);
+
   return (
     <>
       <Menu />
